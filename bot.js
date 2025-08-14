711ec59ef434546a4fc7e061b8e03ecb2a5ef12d
constantinbender51-cmyk <constantinbender51@gmail.com>
2025-08-14 04:48:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -42,7 +42,7 @@ async function runTradingCycle() {
         const riskManager = new RiskManager({ leverage: 10,
     stopLossMultiplier: 2,
     takeProfitMultiplier: 3,
-    marginBuffer: 0.02 });
+    marginBuffer: 0.05 });
         const executionHandler = new ExecutionHandler(dataHandler.api);
 
         //TEST_RUN: START 
